bde6a35d37fdaef635e1e34b4812011ccd43d16e
Mike <stonos@gmail.com>
2014-04-19 20:24:07 +0300

    Add bomb model

---
+++ src/Bomb.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Mike on 19/4/2014.
+ */
+public class Bomb {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Bomb(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+    }
+}
