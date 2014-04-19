51f60e32c14edbcfa310ec1515afe1e5b5f65451
Mike <stonos@gmail.com>
2014-04-19 22:00:35 +0300

    Fix maxDist calculation

--- src/MapController.java
+++ src/MapController.java
@@ -7,36 +7,20 @@ public class MapController {
     private ArrayList<WaspNest> map;
     private double maxDist;
 
-    public MapController() {
+    public MapController(int maxX, int maxY) {
         map = new ArrayList<WaspNest>();
-        maxDist = 0;
+        maxDist = distance(0, 0, maxX, maxY);
     }
 
     public void addWaspNet(WaspNest waspNest) {
         map.add(waspNest);
-        maxDist = calcMaxDist();
+    }
+
+    private double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
     private double distance(ICoordinates point1, ICoordinates point2) {
-        return Math.sqrt(Math.pow((point1.getX() - point2.getX()), 2) + Math.pow((point1.getY() - point2.getY()), 2));
+        return distance(point1.getX(), point1.getY(), point2.getX(), point2.getY());
     }
-
-    private double calcMaxDist() {
-        int size = map.size();
-        double maxDist = 0;
-
-        if (size >= 2) {
-            for(int i = 0; i < size - 1; i++) {
-                for (int j = i + 1; j < size; j++) {
-                    double temp = distance(map.get(i), map.get(j));
-                    if (temp > maxDist) {
-                        maxDist = temp;
-                    }
-                }
-            }
-        }
-
-        return maxDist;
-    }
-
 }
